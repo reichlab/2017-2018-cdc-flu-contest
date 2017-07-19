e23aec5e5773f60a6eb2c9372246ea3d983a96a7
elray1 <evan.l.ray@gmail.com>
2017-07-19 17:20:13 -0400

    update flu_data_with_backfill to pull only the issues that are relevant for prediction in this competition from the Delphi API, and get_partially_revised_ilinet to use only those data

--- R/get-partially-revised-data.R
+++ R/get-partially-revised-data.R
@@ -2,7 +2,11 @@
 
 #' Wrapper function around epiforecast::mimicPastEpidataDF to return partially
 #' revised CDC flu data available as of the specified epiweek (but with missing
-#' values filled in with more recent data from ilinet)
+#' values filled in with more recent data from ilinet).  On top of
+#' epiforecast::mimicPastEpidataDF, this function: 1) uses a static pull of
+#' data from the delphi API, stored in the data directory of this package;
+#' 2) subsets data to a specified region; and 3) drops some occasional extra
+#' rows of NA's at the end of the output from epiforecast::mimicPastEpidataDF.
 #'
 #' @param region_str character defining region of interest, must be in c("nat", paste0("hhs", 1:10))
 #' @param epiweek_str character string defining an epiweek in YYYYWW format
@@ -17,22 +21,14 @@ get_partially_revised_ilinet <- function(region_str, epiweek_str) {
   require(dplyr)
 
   ## from ilinet, via DELPHI API
-  unrevised_data <- readRDS(file.path(
-    find.package("cdcFlu20172018"),
-    "data",
-    "flu_data_with_backfill.rds"))
-
-  ## from ilinet, via cdcfluview package for R
-  final_data <- readRDS(file.path(
-    find.package("cdcFlu20172018"),
-    "data",
-    "flu_data_final_for_prospective_predictions.rds"))
-
-  combined_data <- rbind(unrevised_data, final_data) %>%
+  partially_revised_data <- readRDS(file.path(
+      find.package("cdcFlu20172018"),
+      "data",
+      "flu_data_with_backfill.rds")) %>%
     dplyr::filter(region == region_str)
 
   temp <- epiforecast::mimicPastEpidataDF(
-      combined_data,
+      partially_revised_data,
       epiweek_str) %>%
     dplyr::filter(epiweek <= as.integer(epiweek_str)) %>%
     as.data.frame()
--- inst/data-processing/save-unrevised-data.R
+++ inst/data-processing/save-unrevised-data.R
@@ -4,31 +4,40 @@ library(MMWRweek)
 source("https://raw.githubusercontent.com/cmu-delphi/delphi-epidata/master/src/client/delphi_epidata.R")
 
 # Fetch data
-all_obs <- lapply(c("nat", paste0("hhs", 1:10)),
-    function(region_val) {
-        lapply(0:51,
-            function(lag_val) {
-                obs_one_lag <- Epidata$fluview(
-                    regions = list(region_val),
-                    epiweeks = list(Epidata$range(199740, 201740)),
-                    lag = list(lag_val))
-                
-                lapply(obs_one_lag$epidata,
-                    function(x) {
-                        x[sapply(x, function(comp) is.null(comp))] <- NA
-                        return(as.data.frame(x))
-                    }) %>%
-                    rbind.fill()
-            }) %>%
+all_regions <- c("nat", paste0("hhs", 1:10))
+all_issues <- expand.grid(
+    year = 2010:2017,
+    week = sprintf("%02d", c(1:20, 40:53))
+  ) %>%
+  apply(1, function(x) paste(x, collapse = "")) %>%
+  as.integer()
+all_issues <- all_issues[all_issues <= 201720]
+
+all_obs <- lapply(all_regions,
+  function(region_val) {
+    lapply(all_issues,
+      function(issue_val) {
+        obs_one_issue <- Epidata$fluview(
+          regions = list(region_val),
+          epiweeks = list(Epidata$range(199740, 201740)),
+          issue = list(issue_val))
+
+        lapply(obs_one_issue$epidata,
+          function(x) {
+            x[sapply(x, function(comp) is.null(comp))] <- NA
+            return(as.data.frame(x))
+          }) %>%
             rbind.fill()
-    }) %>%
-    rbind.fill()
+        }) %>%
+          rbind.fill()
+}) %>%
+  rbind.fill()
 
 all_obs$release_date <- as.Date(all_obs$release_date)
-all_obs <- all_obs %>% 
-    separate(epiweek, c("year", "season_week"), sep=4, remove=FALSE) %>%
-    mutate(
-        year = as.integer(year),
-        season_week = as.integer(season_week))
+all_obs <- all_obs %>%
+  separate(epiweek, c("year", "season_week"), sep=4, remove=FALSE) %>%
+  mutate(
+    year = as.integer(year),
+    season_week = as.integer(season_week))
 
 saveRDS(all_obs, file = "data/flu_data_with_backfill.rds")
