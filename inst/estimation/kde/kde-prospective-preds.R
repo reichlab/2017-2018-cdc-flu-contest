de26d8db379cd0c8e4f5ae78f081f618781a9ade
Nicholas G Reich <nick@schoolph.umass.edu>
2017-07-21 14:53:22 -0400

    submission files with full 10000 samples.

--- inst/estimation/kde/kde-prospective-preds.R
+++ inst/estimation/kde/kde-prospective-preds.R
@@ -3,8 +3,9 @@
 ## July 2017
 
 library(cdcFlu20172018)
+library(doMC)
 
-n_sims <- 10
+n_sims <- 10000
 prospective_seasons <- paste0(2010:2016, "/", 2011:2017)
 season_weeks <- 13:42
 
@@ -16,9 +17,10 @@ season_weeks <- 13:42
 ## last predictions due on 5/15/2017 (EW20 == SW 42)
 ## last predictions use data through EW40 == SW18
 ## first_analysis_time_season_week could be set to 15, but padding at front end
+registerDoMC(20)
 
 for(season in prospective_seasons){
-  for(season_week in season_weeks) {
+    foreach(season_week = season_weeks) %dopar% {
     make_one_kde_prediction_file(save_path = "inst/prospective-predictions/kde/",
                                  fits_path = "inst/estimation/kde/fits/",
                                  season = season,
